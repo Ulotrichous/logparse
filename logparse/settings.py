763ed449685beade53090319821494e8d51afa39
Ulotrichous <ryansujanahusein@gmail.com>
2023-01-19 11:58:28 +0700

    Set SECURE_SSL_REDIRECT to True for secure connections

--- logparse/settings.py
+++ logparse/settings.py
@@ -102,6 +102,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 CSRF_TRUSTED_ORIGINS=['logparse.up.railway.app', '*.railway.app']
 
+SECURE_SSL_REDIRECT = True
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
