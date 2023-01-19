cc20f7444f622e8cdf73a65d9a7941ca816095d1
Ulotrichous <ryansujanahusein@gmail.com>
2023-01-19 11:53:12 +0700

    Add CSRF_TRUSTED_ORIGINS to settings for secure cross-origin requests

--- logparse/settings.py
+++ logparse/settings.py
@@ -100,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CSRF_TRUSTED_ORIGINS=['logparse.up.railway.app', '*.railway.app']
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
